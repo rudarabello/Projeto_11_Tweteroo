e210680daa34dcf97ae5936752fdb0b2955ab6fe
rudarabello <ruda.rabello.silva@gmail.com>
2022-06-20 00:10:51 -0300

    feat:fix avatar load

--- index.js
+++ index.js
@@ -10,26 +10,24 @@ const users = [];
 const tweets = [];
 
 app.post('/sign-up', (req, res) => {
-    users.push({
-        username: req.body.username,
-        avatar: req.body.avatar
-    });
+    users.push({...req.body});
     res.status(201).send('OK');
 });
 
 app.post('/tweets', (req, res) => {
+    let usr = req.body.username
+    let loged = users.find(e => e.username === usr)
     tweets.push({
-        username: req.body.username,
+        username: usr,
         tweet: req.body.tweet,
+        avatar: loged.avatar
     });
     res.status(201).send('OK');
 });
 
-
-app.get('/tweets', (req, res) => {    
-    tweets = tweets.slice(-10);
-    res.status(201).send(tweets);
-})
-
+app.get("/tweets", (req, res) => {
+    let data = tweets.slice(-10);
+    res.status(201).send(data);
+});
 
 app.listen(5000);
